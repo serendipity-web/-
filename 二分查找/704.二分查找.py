ea0201a918b10c445a22282c0856ea60058f499d
luozhou <3147527449@qq.com>
2024-08-11 23:02:45 +0800

    [Leetcode 410周赛][第一题][二分查找练习]

---
+++ 二分查找/704.二分查找.py
@@ -0,0 +1,23 @@
+"""
+Day:2024-08-11
+"""
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (right - left) // 2 + left
+            num = nums[mid]
+            if num == target:
+                return mid
+            elif num > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.search())
